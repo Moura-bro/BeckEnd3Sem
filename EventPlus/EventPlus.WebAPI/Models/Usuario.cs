ab186423049bef22f6b0a781978873aedda7b8f4
RAFAEL MOURA BARRIENTO <42501827899@info.local>
2026-03-06 16:48:50 -0300

    Event 06/03/2026

---
+++ EventPlus/EventPlus.WebAPI/Models/Usuario.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace EventPlus.WebAPI.Models;
+
+[Table("Usuario")]
+[Index("Senha", Name = "UQ__Usuario__7ABB9731C466287F", IsUnique = true)]
+[Index("Email", Name = "UQ__Usuario__A9D10534E124C7C6", IsUnique = true)]
+public partial class Usuario
+{
+    [Key]
+    public Guid IdUsuario { get; set; }
+
+    [StringLength(100)]
+    public string Nome { get; set; } = null!;
+
+    [StringLength(256)]
+    public string Email { get; set; } = null!;
+
+    [StringLength(60)]
+    public string Senha { get; set; } = null!;
+
+    public Guid? IdTipoUsuario { get; set; }
+
+    [InverseProperty("IdUsuarioNavigation")]
+
+    [JsonIgnore]
+    public virtual ICollection<ComentarioEvento> ComentarioEventos { get; set; } = new List<ComentarioEvento>();
+
+    [ForeignKey("IdTipoUsuario")]
+    [InverseProperty("Usuarios")]
+    public virtual TipoUsuario? IdTipoUsuarioNavigation { get; set; }
+
+    [InverseProperty("IdUsuarioNavigation")]
+
+    [JsonIgnore]
+    public virtual ICollection<Presenca> Presencas { get; set; } = new List<Presenca>();
+}
